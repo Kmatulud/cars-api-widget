cc06308db2584c0dc7f57ed6bfb1a081934e0a49
kmatulud <princessmakgotso@gmail.com>
2022-04-05 09:00:05 +0200

    fixed handlebars and added styling

--- index.js
+++ index.js
@@ -1,48 +1,88 @@
-const colorElem = document.querySelector(".col");
-const makeElem = document.querySelector(".mak");
-const carElem = document.querySelector(".ca");
-
-const selectColorELem = document.querySelector("#select-colors");
-const selectBrandELem = document.querySelector("#select-brands");
-filterBtnElem = document.querySelector(".filterBtn");
+const main = document.querySelector(".main");
+const carElem = document.querySelector(".cars");
 
 const hbsElem = document.querySelector(".hbsTemplate").innerHTML;
+const tableHbsElem = document.querySelector(".tableHbsTemplate").innerHTML;
+
 const hbsTemplate = Handlebars.compile(hbsElem);
+const tableHbsTemplate = Handlebars.compile(tableHbsElem);
 
 let colors;
 let makes;
 let cars;
 
-
-const getCarData = () => {
-
-    axios
-        .get('https://api-tutor.herokuapp.com/v1/cars')
-        .then(result =>{
-            cars = result.data.splice(0,30);
-            carElem.innerHTML = hbsTemplate({
-                car: cars
-            })
-        })
-    axios
+const getCarData = async() => {
+    await axios
         .get('https://api-tutor.herokuapp.com/v1/makes')
         .then(result =>{
             makes = result.data
-            makeElem.innerHTML = hbsTemplate({
-                make: makes
-            })
-        })
-    axios
+        });
+    await axios
         .get('https://api-tutor.herokuapp.com/v1/colors')
         .then(result =>{
             colors = result.data
-            colorElem.innerHTML = hbsTemplate({
-                color: colors
-            })
-        })
+ 
+        });
+    await axios
+        .get('https://api-tutor.herokuapp.com/v1/cars')
+        .then(result =>{
+            cars = result.data
+        });
 }
 
-getCarData()
+const filterColorBrand = async() => {
 
+const selectColorELem = document.querySelector("#select-colors");
+const selectBrandELem = document.querySelector("#select-brands");
 
+let selectBrand  = selectBrandELem.value;
+let selectColor = selectColorELem.value;
 
+if(selectColor == "all" && selectBrand == "all"){
+    await axios
+        .get('https://api-tutor.herokuapp.com/v1/cars')
+        .then(result => {
+                cars = result.data
+        });
+    }else if(selectBrand != "all" && selectColor == "all" ){
+        await axios
+        .get(`https://api-tutor.herokuapp.com/v1/cars/make/${selectBrand}`)
+        .then(result => {
+                cars = result.data
+        });
+    }else if(selectColor != "all" && selectBrand == "all"){
+        await axios
+        .get(`https://api-tutor.herokuapp.com/v1/cars/color/${selectColor}`)
+        .then(result => {
+                cars = result.data
+        });
+    }else{
+        await axios
+        .get(`https://api-tutor.herokuapp.com/v1/cars/make/${selectBrand}/color/${selectColor}`)
+        .then(result => {
+                cars = result.data 
+        });
+    }
+
+}
+
+const setCarData = async() => {
+    await getCarData();
+    main.innerHTML = hbsTemplate({
+        color: colors,
+        make: makes,
+    });
+    document.querySelector(".cars").innerHTML = tableHbsTemplate({
+        cars: cars.splice(0,30)
+    });
+}
+
+const setFilteredData = async() => {
+    await filterColorBrand();
+    document.querySelector(".cars").innerHTML = tableHbsTemplate({
+        cars: cars.splice(0,30)
+    });
+}
+
+setCarData();
+filterColorBrand();
